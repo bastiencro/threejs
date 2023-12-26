f558af3903626a428dccd3ca38911c4bb79c1da6
Bastien Cro <crozet.bastien1@gmail.com>
2023-12-27 00:59:57 +0100

    my first cube

--- 03-basic-scene/src/script.js
+++ 03-basic-scene/src/script.js
@@ -1,39 +1,41 @@
+console.log('javascript is working');
 import * as THREE from 'three'
+//console.log(THREE);
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+let posX = 0;
 
-// Scene
-const scene = new THREE.Scene()
+//Canva
+const canvas = document.querySelector('canvas.webgl');
 
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-/**
- * Sizes
- */
+//Size
 const sizes = {
-    width: 800,
-    height: 600
+    width : 800,
+    height : 600
 }
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+//Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x00ffff);
 
-/**
- * Renderer
- */
+
+//Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color : 0xff0000});
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = posX;
+
+scene.add(mesh);
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
