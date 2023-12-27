0af3c3d12dc291216c02971b78e77a4c0287733b
Bastien Cro <crozet.bastien1@gmail.com>
2023-12-27 12:13:15 +0100

    End of lesson 5

--- 05-Animation/src/script.js
+++ 05-Animation/src/script.js
@@ -1,5 +1,8 @@
 console.log('javascript is working');
+import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
+
 //console.log(THREE);
 
 
@@ -21,7 +24,6 @@ const axesHelper = new THREE.AxesHelper(2);
 scene.add(axesHelper);
 
 //Object
-// Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
@@ -32,15 +34,50 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
-//camera.lookAt(mesh.position);
 
-//console.log(mesh.position.distanceTo(camera.position))
 
 //Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
 
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+//Animate
+//let time = Date.now(); METHOD WITH DATE
+//const clock = new THREE.Clock(); METHOD WITH TIME
+
+gsap.to(mesh.position, {duration: 1, delay: 1, x: 2}); // METHOD WITH GSAP LIBRARY
+
+
+const tick = () =>
+{
+    //console.log('tick');
+/* METHOD WITH DATE
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time;
+    time = currentTime;
+
+    mesh.rotation.y += .01 * deltaTime; 
+*/
+ /* METHOD WITH TIME
+    const elapsedTime = clock.getElapsedTime();
+   
+    mesh.rotation.y = elapsedTime; 
+
+    mesh.position.x = Math.cos(elapsedTime)
+    mesh.position.y = Math.sin(elapsedTime)
+    
+    camera.position.x = Math.cos(elapsedTime)
+    camera.position.y = Math.sin(elapsedTime)
+    camera.lookAt(mesh.position)
+*/
+
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+};
+
+tick();
+
+
 
